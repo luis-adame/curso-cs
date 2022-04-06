36bd70016738398d7a78aa34258f5ea6f2f0c4c1
Luis <Luis@DESKTOP-TDGOJHH>
2022-04-06 07:57:18 -0800

    Primeros metodos de validacion.

--- ejercicios-20220406/Validaciones/Program.cs
+++ ejercicios-20220406/Validaciones/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.Text.RegularExpressions;
+
+static void DomainValidation()
+{
+	var domain = "https://www.something.com";
+	Regex regex = new Regex(@"^https?://(www.)?([\w]+)((\.[A-Za-z]{2,3})+)$");
+}
+
+static void phoneNumber()
+{
+	var phoneNumber = "+52 (686) 405 4720";
+	Regex regex = new Regex(@"^+[\d]{2}\s\([\d]{3}\)\s[\d]{3}\s[\d]{4}");
+	Console.WriteLine(regex.IsMatch(phoneNumber));
+}
