d3654ccf250cef7ae83b7229b8a32885d51bf1ae
Luis <Luis@DESKTOP-TDGOJHH>
2022-04-06 12:14:15 -0800

    Wait for it

--- ejercicios-20220406/Asynchronous/Program.cs
+++ ejercicios-20220406/Asynchronous/Program.cs
@@ -1,2 +1,56 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Diagnostics;
+//Console.WriteLine("Loading...");
+//Asynchronous.WaitForIt();
+//await Asynchronous.WaitForIt2();
+
+var timeElapsed = await Parallelism.Main();
+Console.WriteLine($"Both proceses finished after {timeElapsed / 1000m} seconds");
+
+public class Asynchronous
+{
+	//Asincrono
+	public static async Task WaitForIt()
+	{
+		await Task.Delay(TimeSpan.FromSeconds(5));
+		Console.WriteLine("Five seconds complete");
+	}
+
+	public static async Task WaitForIt2()
+	{
+		await Task.Delay(10000);
+		Console.WriteLine("Ten seconds complete");
+	}
+}
+
+public class Parallelism
+{
+	public static async Task<long> Main()
+	{
+		var stopwatch = new Stopwatch();
+		stopwatch.Start();
+
+		Process1();
+		Process2();
+
+		stopwatch.Stop();
+
+		return stopwatch.ElapsedMilliseconds;
+	}
+
+	public static async Task Process1()
+	{
+		await Task.Run(() =>
+		{
+			Thread.Sleep(4000);
+		});
+	}
+
+	public static async Task Process2()
+	{
+		await Task.Run(() =>
+		{
+			Thread.Sleep(1000);
+		});
+	}
+}
