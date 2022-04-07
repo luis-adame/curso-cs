d1634f0e46f8753e6d1b581921a02c709567edf0
Luis <Luis@DESKTOP-TDGOJHH>
2022-04-07 08:19:24 -0800

    Primeros ejericicos herencia

--- ejercicios-20220407/POO/Program.cs
+++ ejercicios-20220407/POO/Program.cs
@@ -1,2 +1,46 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+
+class Polygon
+{
+	public decimal Base { get; set; }
+	public decimal Height { get; set; }
+
+	public virtual decimal GetArea()
+	{
+		return Base * Height;
+	}
+}
+
+class Square : Polygon
+{
+	public Square(decimal baseDim)
+	{
+		Base = baseDim;
+		Height = baseDim;
+	}
+}
+
+class Rectangle : Polygon
+{
+	public Rectangle(int baseDim, int heightDim)
+	{
+		Base = baseDim;
+		Height = heightDim;
+	}
+}
+
+class Triangle : Polygon
+{
+	public Triangle(int baseDim, int heightDim)
+	{
+		Base = baseDim;
+		Height = heightDim;
+	}
+
+	public override decimal GetArea()
+	{
+		return Base * Height / 2;
+	}
+}
